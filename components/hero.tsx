b14c0b9cf6d79b2b669c55da02ab6b2f3c4ef41a
Piyush Modgil <piyushmodgil9@gmail.com>
2025-04-14 15:46:23 +0530

    manages works

--- components/hero.tsx
+++ components/hero.tsx
@@ -28,7 +28,7 @@ export default function Hero() {
           animate={{ opacity: 1 }}
           transition={{ delay: 0.4, duration: 0.8 }}
         >
-          Developer & Tech Enthusiast
+          CTO & Cofounder of Esprit System | Developer & Tech Enthusiast
         </motion.p>
 
         <motion.div
